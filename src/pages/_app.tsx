352cc1064420d444be2ecd04fab0171383d8c9e0
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-18 12:37:20 -0300

    Add Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
+import { Container, Header } from '@/styles/app'
 import { GlobalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import logoImg from '../assets/logo.svg'
 
 GlobalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
