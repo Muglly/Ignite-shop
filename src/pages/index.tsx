bbdc7e3839b89d86fd7983b13bd3001e5beb961f
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-19 20:55:14 -0300

    Add slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,59 @@
-import { HomeContainer, Product } from '@/styles/pages/home'
 import Image from 'next/image'
+
+import { useKeenSlider } from 'keen-slider/react'
 
 import Camiseta1 from '../assets/camisetas/1.png'
 import Camiseta2 from '../assets/camisetas/2.png'
 import Camiseta3 from '../assets/camisetas/3.png'
 import Camiseta4 from '../assets/camisetas/4.png'
 
+import * as S from '@/styles/pages/home'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <S.HomeContainer ref={sliderRef} className="keen-slider">
+      <S.Product className="keen-slider__slide">
         <Image src={Camiseta1} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta x</strong>
           <span>R$ 79,90</span>
         </footer>
-      </Product>
+      </S.Product>
 
-      <Product>
+      <S.Product className="keen-slider__slide">
         <Image src={Camiseta2} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta y</strong>
           <span>R$ 79,90</span>
         </footer>
-      </Product>
-    </HomeContainer>
+      </S.Product>
+
+      <S.Product className="keen-slider__slide">
+        <Image src={Camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </S.Product>
+
+      <S.Product className="keen-slider__slide">
+        <Image src={Camiseta4} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </S.Product>
+    </S.HomeContainer>
   )
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,11 +2,10 @@ import { styled } from '..'
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
-
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) /2))',
   marginLeft: 'auto',
+  overflow: 'hidden',
 })
 
 export const Product = styled('a', {
