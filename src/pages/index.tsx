facc8f39da59c19b5d12d181baea0cf8463dcf32
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-23 18:33:58 -0300

    getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { stripe } from '@/lib/stripe'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import { useKeenSlider } from 'keen-slider/react'
 
@@ -47,7 +47,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   })
@@ -67,5 +67,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2,
   }
 }
