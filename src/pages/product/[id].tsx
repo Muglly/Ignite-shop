1b01622328c1add916d7f6e1064229ec13ae418f
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-25 09:16:26 -0300

    Add Product page html structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,23 @@
-import React from 'react'
+import * as S from '@/styles/pages/product'
 
 export default function Product() {
-  return <div>[id]</div>
+  return (
+    <S.ProductContainer>
+      <S.ImageContainer></S.ImageContainer>
+
+      <S.ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 70,90</span>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
+          incidunt dolorum neque. Explicabo, voluptatibus iusto itaque sint amet
+          sapiente dignissimos quaerat maiores sit, sed ad laborum, suscipit
+          architecto illum? Corrupti.
+        </p>
+
+        <button>Comprar agora</button>
+      </S.ProductDetails>
+    </S.ProductContainer>
+  )
 }
