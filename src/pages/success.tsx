f9efa82d741ad175b8295ef06f77e83fd36f26de
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-31 22:59:50 -0300

    Add success page structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,20 @@
+import * as S from '@/styles/pages/success'
+import Link from 'next/link'
+
 export default function success() {
-  return <div>success</div>
+  return (
+    <S.SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <S.ImageContainer></S.ImageContainer>
+
+      <p>
+        Uhuul <strong>Rafael Souza</strong>Rafael Souza, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </S.SuccessContainer>
+  )
 }
